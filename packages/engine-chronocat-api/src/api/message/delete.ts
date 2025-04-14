9a2bb44c70c895c0535fbd93b298f8b74b36e333
42 <42@koishi.chat>
2025-04-15 01:38:09 +0800

    fix(api): fix recall

--- packages/engine-chronocat-api/src/api/message/delete.ts
+++ packages/engine-chronocat-api/src/api/message/delete.ts
@@ -5,20 +5,26 @@ import { recallMsg } from '../../definitions/msgService'
 export const buildMessageDelete =
   (ctx: ChronocatContext) =>
   async ({ channel_id, message_id }: MessageDeletePayload) => {
-    const privatePeerUid = await ctx.chronocat.uix.getUid2(channel_id.slice(8)) // private:
-    if (!privatePeerUid) {
-      ctx.chronocat.l.error('内部错误', {
-        code: 2152,
-        throw: true,
-      })
-      return {}
+    const isPrivate = channel_id.startsWith('private:')
+    
+    let privatePeerUid: string | undefined
+    
+    if (isPrivate) {
+      privatePeerUid = await ctx.chronocat.uix.getUid2(channel_id.slice(8))
+      if (!privatePeerUid) {
+        ctx.chronocat.l.error('内部错误', {
+          code: 2152,
+          throw: true,
+        })
+        return {}
+      }
     }
 
     await recallMsg({
-      peer: channel_id.startsWith('private:')
+      peer: isPrivate
         ? {
             chatType: ChatType.Private,
-            peerUid: privatePeerUid,
+            peerUid: privatePeerUid!,
           }
         : {
             chatType: ChatType.Group,
